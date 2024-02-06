e2bd906b492be62a47132d9c3a99012afa5a04af
publicqi <qisu@ucsb.edu>
2024-02-06 22:40:38 +0800

    add panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
+use std::sync::Arc;
+use tokio;
+
+use ethers_providers::{Http, Middleware, Provider};
+use revm::db::EthersDB;
+
+#[tokio::main]
+async fn main() {
+    let url = "http://localhost:8124/eth";
+    let client = Provider::<Http>::try_from(url).unwrap();
+
+    let block_number = client.get_block_number().await.unwrap();
+    println!("block_number: {:?}", block_number);
+
+    let _ = EthersDB::new(Arc::new(client), None).unwrap();
+
+    panic!("we should panic");
+}
+
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
-
-    use revm::db::EthersDB;
-
+    use super::*;
     #[tokio::test]
     async fn test_hang() {
-        use ethers_providers::{Http, Middleware, Provider};
-
         let url = "http://localhost:8124/eth";
         let client = Provider::<Http>::try_from(url).unwrap();
 
